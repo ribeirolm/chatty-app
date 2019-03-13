067cf6d684161c2dbcb19ad84e34943132c804db
Lauren <laurenribeiro06@gmail.com>
2019-03-13 16:46:55 +0000

    Created a websocket server and sent messages to the server

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,34 @@ class App extends Component {
             content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
           }
         ]
-      }
+      };
+  }
+  ws = new WebSocket('ws://0.0.0.0:3001');
+  componentDidMount() {
+    this.ws.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log('connected to server')
+    }
+
+    this.ws.onmessage = evt => {
+      // on receiving a message, add it to the list of messages
+      const message = JSON.parse(evt.data)
+      this.addMessage(message)
+    }
+
+    this.ws.onclose = () => {
+      console.log('disconnected')
+    }
   }
 
   addMessage = (content) => {
       // Add a new message to the list of messages in the data store
     const newMessage = {username: this.state.currentUser.name, content: content};
-    const messages = this.state.messages.concat(newMessage)
+    // const messages = this.state.messages.concat(newMessage)
       // Update the state of the app component.
       // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
+    // this.setState({messages: messages})
+    this.ws.send(JSON.stringify(newMessage));
   };
 
   updateUsername = (username) => {
--- src/Message.jsx
+++ src/Message.jsx
@@ -11,9 +11,9 @@ class Message extends Component {
     <span className="message-content">{this.props.content}</span>
   </div>
   
-  <div className="message system">
+  {/* <div className="message system">
     Anonymous1 changed their name to nomnom.
-  </div>
+  </div> */}
       </div>
       
     );
